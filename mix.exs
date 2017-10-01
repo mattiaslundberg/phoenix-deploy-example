6836647e3439e0125a0ee96ecfc27ea91c4cf599
Mattias Lundberg <me@mlundberg.se>
2017-10-01 15:31:23 +0200

    Move current version to file for easier updating

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HelloRelease.Mixfile do
   def project do
     [
       app: :hello_release,
-      version: "0.0.12",
+      version: String.trim(File.read!("VERSION")),
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
